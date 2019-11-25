eee59fd6f84a269893ee3e8f16da8e377abaafda
abondarenko <a.bondarenko@carprice.ru>
2019-11-26 00:03:52 +0300

    added yandex poll_worker question

--- make_tasks.sh
+++ make_tasks.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
-#TODO
+#It's horrible, I know.
+#But it just works.
 TASK=$1
 REPOSITORY=$2
-SERVICES=$3
+SERVICE=$3
 
 cd $REPOSITORY/devserver/tasks
 
-if [ $SERVICES == monolith ];
+if [ $SERVICE == monolith ];
   then
     echo "monolith:" >> $TASK.yaml
     echo "   git_branch: \"$TASK\"" >> $TASK.yaml
@@ -15,56 +16,61 @@ if [ $SERVICES == monolith ];
     cat $TASK.yaml
     echo ""
 fi
-if [ $SERVICES == admin-documents-front ];
+if [ $SERVICE == admin-documents-front ];
   then
     echo "admin_documents_front:" >> $TASK.yaml
-    echo "  nginx_image: \"nexus.carprice.ru/admin-documents-front:$TASK\"" >> $TASK.yaml
+    echo "  nginx_image: \"nexus.carprice.ru/$SERVICE:$TASK\"" >> $TASK.yaml
     echo ""
     echo $TASK.yaml
     cat $TASK.yaml
     echo ""
 fi
-if [ $SERVICES == bpm-exchanger ];
+if [ $SERVICE == bpm-exchanger ];
   then
     echo "bpm_exchanger:" >> $TASK.yaml
-    echo "  fpm_image: \"nexus.carprice.ru/bpm-exchanger/php:$TASK\"" >> $TASK.yaml
+    echo "  fpm_image: \"nexus.carprice.ru/$SERVICE/php:$TASK\"" >> $TASK.yaml
     echo ""
     echo $TASK.yaml
     cat $TASK.yaml
     echo ""
 fi
-if [ $SERVICES == bpm-docflow ];
+if [ $SERVICE == bpm-docflow ];
   then
     echo "bpm-docflow:" >> $TASK.yaml
-    echo "  fpm_image: \"nexus.carprice.ru/bpm-docflow/fpm:$TASK\"" >> $TASK.yaml
+    echo "  fpm_image: \"nexus.carprice.ru/$SERVICE/fpm:$TASK\"" >> $TASK.yaml
     echo ""
     echo $TASK.yaml
     cat $TASK.yaml
     echo ""
 fi
-if [ $SERVICES == papi-dev ];
+if [ $SERVICE == papi-dev ];
   then
     echo "papi_dev:" >> $TASK.yaml
     echo "  fpm_image: \"nexus.carprice.ru/papi/fpm:$TASK\"" >> $TASK.yaml
-    echo "  nginx_image: \"nexus.carprice.ru/papi/nginx:$TASK\"" >> $TASK.yaml
     echo ""
     echo $TASK.yaml
     cat $TASK.yaml
     echo ""
 fi
-if [ $SERVICES == services ];
+if [ $SERVICE == services ];
   then
+    function pollworker() {
+        echo "  yandex:" >> $TASK.yaml
+        echo "    poll_worker_enabled: true" >> $TASK.yaml
+    }
     echo "services:" >> $TASK.yaml
-    echo "  image: \"nexus.carprice.ru/go/services:$TASK\"" >> $TASK.yaml
-    echo "  yandex:" >> $TASK.yaml
-    #TODO
-    echo "    poll_worker_enabled: true" >> $TASK.yaml
+    echo "  image: \"nexus.carprice.ru/go/$SERVICE:$TASK\"" >> $TASK.yaml
+    read -p "Do you wish yandex poll_worker to be enabled? yn" yn
+    case $yn in
+        [Yy]* ) pollworker; break;;
+        * ) echo "poll_worker is disabled!";;
+    esac
     echo ""
     echo $TASK.yaml
     cat $TASK.yaml
     echo ""
 fi
-if [ $SERVICES == accounts ];
+if [ $SERVICE == accounts ];
   then
     echo "accounts:" >> $TASK.yaml
     echo "  image: \"nexus.carprice.ru/go/accounts:$TASK\"" >> $TASK.yaml
